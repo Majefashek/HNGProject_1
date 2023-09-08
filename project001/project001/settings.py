b3f77fce4bbc1e32f7c49b58af914f6de3791f2b
unknown <abdullahishuaibumaje.com>
2023-09-08 08:18:55 -0700

    changed to nigerian timezone

--- project001/project001/settings.py
+++ project001/project001/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 #ALLOWED_HOSTS = ['https://get-slack-details-endpoint.onrender.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
